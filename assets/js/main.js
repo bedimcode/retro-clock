dd16e50f584f2904c9eb652870142d2f755c9b5f
Marlon <61248995+bedimcode@users.noreply.github.com>
2023-05-19 09:19:56 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=============== CLOCK ===============*/
+const hour = document.getElementById('clock-hour'),
+      minutes = document.getElementById('clock-minutes')
+
+const clock = () =>{
+   
+}
+setInterval(clock, 1000) // (Updates every 1s) 1000 = 1s 
+
+/*=============== TIME AND DATE TEXT ===============*/
+const dateDayWeek = document.getElementById('date-day-week'),
+      dateMonth = document.getElementById('date-month'),
+      dateDay = document.getElementById('date-day'),
+      dateYear = document.getElementById('date-year'),
+      textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm')
+
+const clockText = () =>{
+   
+}
+setInterval(clockText, 1000) // (Updates every 1s) 1000 = 1s
